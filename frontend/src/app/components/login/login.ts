c1c1aed44b2a2d9815c8087b014be7ae24def143
Phadtare <Santosh.Phadtare@saint-gobain.com>
2025-09-09 17:08:11 +0530

    Login Component

---
+++ frontend/src/app/components/login/login.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class Login {
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    // Handle login logic here
+    console.log('Login successful', this.loginForm.value);
+  }
+}
