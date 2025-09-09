af1976920a1356f46a8d51310dddab3c2f1ab7fd
psantosh16 <santoshphadtare1610@gmail.com>
2025-09-09 23:19:24 +0530

    feat: updated routes for dashboard and change request components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
+import { Dashboard } from './components/dashboard/dashboard';
+import { SubmitChangeRequest } from './components/submit-change-request/submit-change-request';
 
 export const routes: Routes = [
     {
-        path: "", redirectTo: 'login', pathMatch: "full"
+        path: "", redirectTo: 'dashboard', pathMatch: "full"
     },
     {
         path: "login", component: Login
+    },
+    {
+        path: "dashboard", component: Dashboard
+    },
+    {
+        path: "change-request", component: SubmitChangeRequest
     }
 ];
